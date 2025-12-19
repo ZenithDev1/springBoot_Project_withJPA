7a89f4f3cd8d1faac4a020b0b775b4662f70862e
ZenithDev1 <devendra702321@gmail.com>
2025-12-19 23:16:57 +0530

    User Role created

---
+++ hospitalManagement/src/main/java/com/zenithdev/linkedin/hospitalManagement/entity/User.java
@@ -0,0 +1,34 @@
+package com.zenithdev.linkedin.hospitalManagement.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @JoinColumn(unique = true)
+    private String username;
+
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+}

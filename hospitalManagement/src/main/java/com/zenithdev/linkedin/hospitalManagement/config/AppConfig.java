9aa248a7eb6adc7de622a1a9432283646353669b
ZenithDev1 <devendra702321@gmail.com>
2025-12-19 19:08:10 +0530

    Role based access implemented

--- hospitalManagement/src/main/java/com/zenithdev/linkedin/hospitalManagement/config/AppConfig.java
+++ hospitalManagement/src/main/java/com/zenithdev/linkedin/hospitalManagement/config/AppConfig.java
@@ -1,8 +1,11 @@
 package com.zenithdev.linkedin.hospitalManagement.config;
 
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +13,10 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- hospitalManagement/src/main/java/com/zenithdev/linkedin/hospitalManagement/config/WebSecurityConfig.java
+++ hospitalManagement/src/main/java/com/zenithdev/linkedin/hospitalManagement/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.zenithdev.linkedin.hospitalManagement.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,11 +8,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final PasswordEncoder passwordEncoder;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -28,12 +33,12 @@ public class WebSecurityConfig {
     @Bean
     UserDetailsService userDetailsService() {
         UserDetails user1 = User.withUsername("admin")
-                .password("adminpass")
+                .password(passwordEncoder.encode("adminpass"))
                 .roles("ADMIN")
                 .build();
 
         UserDetails user2 = User.withUsername("patient")
-                .password("patientpass")
+                .password(passwordEncoder.encode("patientpass"))
                 .roles("PATIENT")
                 .build();
 
